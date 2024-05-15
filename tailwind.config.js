0e55f75ecddf95cdb1b0bb56f43442010077d776
unknown <rajeruk@gmail.com>
2024-05-15 09:47:20 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./public/**/*.{html,js}'],
+  theme: {
+    extend: {
+      colors: {
+        cUtama : '#14b8a6',
+        cDark : '#0f172a',
+        csecond: '#64748b',
+        cColor1: '#FFF67E',
+        cColor2: '#BFEA7C',
+        cColor3: '#9BCF53',
+        cColor4: '#416D19'
+      }
+    },
+  },
+  plugins: [],
+}
+
